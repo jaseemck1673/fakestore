82dda4aadea29b6565675a39c8ec091e6055211c
Jaseem <jaseemkunji2000@gmail.com>
2025-02-06 21:52:43 +0530

    all category and products are showened in home page

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import requests
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'index.html')
+    url = "https://fakestoreapi.com/products"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        products = response.json()
+        category_product = {}
+        
+        for product in products:
+            category = product['category']
+            if category not in category_product:
+                category_product[category] = product
+            
+        filtered_products = list(category_product.values())
+        print(filtered_products)
+        return render(request,'index.html',{'categories':filtered_products,'products':products})
+    else:
+        return HttpResponse("Error",status=response.status_code)
+    
+    
